d31e3f574405a8e822af8b6cc28ac53b6ae36cd9
sbx_user1059 <sbx_user1059@ip-10-129-62-206.eu-west-2.compute.internal>
2018-09-10 13:36:17 +0000

    Mon Sep 10 13:36:17 UTC 2018

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -3,8 +3,10 @@ package befaster.solutions.HLO;
 import befaster.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
+    //returns greeting to the user
+    //friendName is a parameter responsible for friend's name
     public String hello(String friendName) {
 
-       return "Hello, World!";
+       return "Hello, " + friendName;
     }
 }
