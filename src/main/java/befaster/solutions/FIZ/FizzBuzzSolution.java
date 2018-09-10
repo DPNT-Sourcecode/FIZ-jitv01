68331a13dd1b2b853657b532b8e4d11e6d0aa92f
sbx_user1059 <sbx_user1059@ip-10-128-50-44.eu-west-2.compute.internal>
2018-09-10 14:08:17 +0000

    Mon Sep 10 14:08:17 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,17 +4,23 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzzSolution {
 
+    //method that returns a message depending on the input number
+
     public String fizzBuzz(Integer number) {
 
+        //CASE number % 3 == 0 AND number % 5 == 0
         if ((number % 3 == 0) && (number % 5 == 0))
             return "fizz buzz";
 
+        //CASE number % 3 == 0
         if (number % 3 == 0)
             return "fizz";
 
+        //CASE number % 5 == 0
         if (number % 5 == 0)
             return "buzz";
 
+        //number is not divisible by neither 3 or 5
         return String.valueOf(number);
 
     }
